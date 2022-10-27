052a669ab01ca264e3016a176f8d17bf05148005
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-27 22:32:14 +0100

    Update dashboard_menu.dart

--- lib/widgets/dashboard_menu.dart
+++ lib/widgets/dashboard_menu.dart
@@ -9,22 +9,27 @@ class dashboard extends StatefulWidget {
   State<dashboard> createState() => _dashboardState();
 }
 
-class _dashboardState extends State<dashboard>  with SingleTickerProviderStateMixin{
+class _dashboardState extends State<dashboard>
+    with SingleTickerProviderStateMixin {
   bool isCollapsed = true;
   late double screenWidth, screenHeight;
   final Duration duration = const Duration(milliseconds: 300);
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
 
-  
   @override
   void initState() {
     super.initState();
-    
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.6).animate(_controller);
   }
 
-  @override 
+  @override
   void dispose() {
-    super.dispose(); 
+    _controller.dispose();
+    super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -32,67 +37,76 @@ class _dashboardState extends State<dashboard>  with SingleTickerProviderStateMi
     screenHeight = size.height;
     return AnimatedPositioned(
       duration: duration,
-      top: isCollapsed ? 0 : 0.2 * screenHeight,
+      top:
+          0, // isCollapsed ? 0 : 0.2 * screenHeight, - now removed after the tween begin and end has been implemented, same as the bottom.
       left: isCollapsed ? 0 : 0.6 * screenWidth,
-      bottom: isCollapsed ? 0 : 0.2 * screenWidth,
+      bottom: 0, //  isCollapsed ? 0 : 0.2 * screenWidth,
       right: isCollapsed ? 0 : -0.4 * screenWidth,
-      child: Material(
-          borderRadius: BorderRadius.circular(20),
-          // Alternatively written : borderRadius: BorderRadius.all(Radius.circular(40)),
-          elevation: 5,
-          color: backgroundColor,
-          child: Container(
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    InkWell(
-                      child: const Icon(Icons.menu, color: Colors.white),
-                      onTap: () {
-                        setState(() {
-                          isCollapsed = !isCollapsed;
-                        });
-                      },
-                    ),
-                    const Text(
-                      'My Cards',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                    const Icon(Icons.settings, color: Colors.white),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                SizedBox(
-                  height: 200,
-                  child: PageView(
-                    controller: PageController(viewportFraction: 0.8),
-                    scrollDirection: Axis.horizontal,
-                    pageSnapping: true,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Material(
+            borderRadius: BorderRadius.circular(20),
+            // Alternatively written : borderRadius: BorderRadius.all(Radius.circular(40)),
+            elevation: 5,
+            color: backgroundColor,
+            child: Container(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
                     children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.redAccent,
-                        width: 100,
+                      InkWell(
+                        child: const Icon(Icons.menu, color: Colors.white),
+                        onTap: () {
+                          setState(() {
+                            if (isCollapsed) {
+                              _controller.forward();
+                            } else {
+                              _controller.reverse();
+                            }
+                            isCollapsed = !isCollapsed;
+                          });
+                        },
                       ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.blueAccent,
-                        width: 100,
+                      const Text(
+                        'My Cards',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
                       ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.greenAccent,
-                        width: 100,
-                      ),
+                      const Icon(Icons.settings, color: Colors.white),
                     ],
                   ),
-                )
-              ],
-            ),
-          )),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction: 0.8),
+                      scrollDirection: Axis.horizontal,
+                      pageSnapping: true,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
