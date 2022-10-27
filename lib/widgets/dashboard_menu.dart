384bad6529cf8b1c280421c8a42e259b072bebd4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-27 21:19:35 +0100

    updating dashboard menu

--- lib/widgets/dashboard_menu.dart
+++ lib/widgets/dashboard_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constant.dart';
+
 class dashboard extends StatefulWidget {
   const dashboard({super.key});
 
@@ -10,6 +12,15 @@ class dashboard extends StatefulWidget {
 class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+        elevation: 8,
+        color: backgroundColor,
+        child: Column(
+          children: [
+            Row(
+              children: const [],
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/nav_menu.dart
+++ lib/widgets/nav_menu.dart
@@ -10,29 +10,42 @@ class navmenu extends StatefulWidget {
 class _navmenuState extends State<navmenu> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Text(
-          'Dashboard',
-          style: TextStyle(color: Colors.white, fontSize: 20),
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Dashboard',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Messages',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Utlity Bills',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Funds Transfer',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Branches',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
         ),
-        Text(
-          'Messages',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        Text(
-          'Utlity Bills',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        Text(
-          'Funds Transfer',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        Text(
-          'Branches',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ],
+      ),
     );
   }
 }
