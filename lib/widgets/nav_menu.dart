56384513ca9d04d7b5ef0eb9803ee035acabc528
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-27 21:12:40 +0100

    Update nav_menu.dart

--- lib/widgets/nav_menu.dart
+++ lib/widgets/nav_menu.dart
@@ -11,7 +11,28 @@ class _navmenuState extends State<navmenu> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      
+      children: const [
+        Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Text(
+          'Messages',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Text(
+          'Utlity Bills',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Text(
+          'Funds Transfer',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Text(
+          'Branches',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ],
     );
   }
 }
