8cca7cb397408a66a327fc8c3441ae3393f0b352
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-27 21:04:25 +0100

    adding necessary widgets

--- lib/widgets/menu_dashboard.dart
+++ lib/widgets/menu_dashboard.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:sidemenu_dashboard/widgets/constant.dart';
+
+import 'dashboard_menu.dart';
+import 'nav_menu.dart';
 
 class MenuDashboard extends StatelessWidget {
   const MenuDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Stack(children: const [
+        navmenu(),
+        dashboard(),
+      ]),
+    );
   }
 }
---
+++ lib/widgets/nav_menu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class navmenu extends StatefulWidget {
+  const navmenu({super.key});
+
+  @override
+  State<navmenu> createState() => _navmenuState();
+}
+
+class _navmenuState extends State<navmenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
